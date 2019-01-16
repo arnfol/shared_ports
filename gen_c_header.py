2323af7d444de3ccf9039261316cb377c7dbacdb
arnfol <arnfol@mail.ru>
2019-01-16 23:08:28 +0300

    add struct generation and examples

--- gen_c_header.py
+++ gen_c_header.py
@@ -35,8 +35,9 @@ header_template = """
 // ----------------------------------------------------------------
 // Registers
 // ----------------------------------------------------------------
+
 typedef struct mux_type {{
-    {registers}
+{registers}
 }} mux_type;
 
 #define MUX ((mux_type*)SHARED_PINS_ADDR)
@@ -45,17 +46,27 @@ typedef struct mux_type {{
 
 def main():
 
+    # ------------------------------------------------------------------------------------
+    # read table
+    # ------------------------------------------------------------------------------------
     psig_list, isig_list = gen_shared_pins.read_table(gen_shared_pins.inp_file, gen_shared_pins.head_lines)
 
     enums = ""
+    registers = ""
+
     for p in psig_list:
         isigs = []
 
+        # generate peripherial signal name
         if p['bit'] is None:
             psig = p['name']
         else:
             psig = p['name'] + '_' + p['bit']
 
+        if p is psig_list[0]: 
+            example_psig = psig
+
+        # generate list of internal signal names
         for i in p['connections']:
             if i is not None:
                 if i['bit'] is None:
@@ -63,9 +74,14 @@ def main():
                 else:
                     isigs.append(i['name'] + '_' + i['bit'])
 
-        enums += enum_str.format(psig=psig, isigs=', '.join(isigs)) + '\n'
+        if p is psig_list[0]:
+            example_isig = isigs[0]
 
-    print(enums+'\n')
+        enums += '\n' + enum_str.format(psig=psig, isigs=', '.join(isigs))
+        registers += '\n' + '\t' + struct_str.format(psig=psig)
+
+
+
 
     # ------------------------------------------------------------------------------------
     # generate output file 
@@ -75,11 +91,11 @@ def main():
     result = header_template.format(
         date=gen_shared_pins.date,
         author=gen_shared_pins.author,
-        example_psig="",
-        example_isig="",
+        example_psig=example_psig,
+        example_isig=example_isig,
         addr=shared_pins_apb_addr,
-        enums="",
-        registers=""
+        enums=enums,
+        registers=registers
         )
 
     with open(out_file,'w') as out:
--- shared_pins.h
+++ shared_pins.h
@@ -2,11 +2,11 @@
 /*
     This is an automatically generated file.
 
-    Date: 2019-01-16 22:51
+    Date: 2019-01-16 23:07
     Author: arnfo
 
     Usage:
-    MUX-> = _opt.;
+    MUX->cmn_port1_0 = cmn_port1_0_opt.gpio_14;
 
 */
 
@@ -17,12 +17,85 @@
 // Register types
 // ----------------------------------------------------------------
 
+enum { gpio_14, can1_tx, i2c1_scl, uart1_rx } cmn_port1_0_opt;
+enum { gpio_15, can1_rx, i2c1_sda, uart1_tx } cmn_port1_1_opt;
+enum { gpio_12, can1_tx, i2c2_scl, uart2_rx } cmn_port1_2_opt;
+enum { gpio_13, can1_rx, i2c2_sda, uart2_tx } cmn_port1_3_opt;
+enum { gpio_8, can2_tx, i2c2_scl, uart1_rx } cmn_port1_4_opt;
+enum { gpio_9, can2_rx, i2c2_sda, uart1_tx } cmn_port1_5_opt;
+enum { gpio_10, can2_tx, i2c1_scl, uart2_rx } cmn_port1_6_opt;
+enum { gpio_11, can2_rx, i2c1_sda, uart2_tx } cmn_port1_7_opt;
+enum { gpio_16, i2c2_scl, uart2_rx, spi2_sel } cmn_port_12_opt;
+enum { gpio_17, i2c2_sda, uart2_tx, spi2_miso } cmn_port_13_opt;
+enum { gpio_18, i2c1_scl, jtagmst_tdi, spi2_mosi } cmn_port_14_opt;
+enum { gpio_19, i2c1_sda, jtagmst_tdo, spi2_sck } cmn_port_15_opt;
+enum { gpio_20, spi2_sel, jtagmst_tck, spi1_sel } cmn_port_16_opt;
+enum { gpio_21, spi2_miso, jtagmst_tms, spi1_miso } cmn_port_17_opt;
+enum { gpio_22, spi2_mosi, uart1_rx, spi1_mosi } cmn_port_18_opt;
+enum { gpio_23, spi2_sck, uart1_tx, spi1_sck } cmn_port_19_opt;
+enum { gpio_24, uart1_rx, spi2_sel, gpio_4 } cmn_port_20_opt;
+enum { gpio_25, uart1_tx, spi2_miso, gpio_5 } cmn_port_21_opt;
+enum { gpio_26, can1_tx, spi2_mosi, gpio_6 } cmn_port_22_opt;
+enum { gpio_27, can1_rx, spi2_sck, gpio_7 } cmn_port_23_opt;
+enum { gpio_28, uart2_rx, spi1_sel, gpio_8 } cmn_port_24_opt;
+enum { gpio_29, uart2_tx, spi1_miso, gpio_9 } cmn_port_25_opt;
+enum { gpio_30, can2_tx, spi1_mosi, gpio_10 } cmn_port_26_opt;
+enum { gpio_31, can2_rx, spi1_sck, gpio_11 } cmn_port_27_opt;
+enum { gpio_32, i2c1_scl, jtagmst_tdi, gpio_16 } cmn_port_28_opt;
+enum { gpio_33, i2c1_sda, jtagmst_tdo, gpio_17 } cmn_port_29_opt;
+enum { gpio_34, jtagmst_tck, gpio_18 } cmn_port_30_opt;
+enum { gpio_35, jtagmst_tms, gpio_19 } cmn_port_31_opt;
+enum { gpio_36, jtagmst_tdi } cmn_port_32_opt;
+enum { gpio_37, jtagmst_tdo } cmn_port_33_opt;
+enum { gpio_38, i2c2_scl, jtagmst_tck } cmn_port_34_opt;
+enum { gpio_39, i2c2_sda, jtagmst_tms } cmn_port_35_opt;
+enum { gpio_40, i2c1_scl } extended_addr_1_opt;
+enum { gpio_41, i2c1_sda } ext_cen_2_opt;
+enum { gpio_42, i2c2_scl } sp_pin0_opt;
+enum { gpio_43, i2c2_sda } sp_pin1_opt;
 
 // ----------------------------------------------------------------
 // Registers
 // ----------------------------------------------------------------
+
 typedef struct mux_type {
-    
+
+	unsigned char cmn_port1_0;
+	unsigned char cmn_port1_1;
+	unsigned char cmn_port1_2;
+	unsigned char cmn_port1_3;
+	unsigned char cmn_port1_4;
+	unsigned char cmn_port1_5;
+	unsigned char cmn_port1_6;
+	unsigned char cmn_port1_7;
+	unsigned char cmn_port_12;
+	unsigned char cmn_port_13;
+	unsigned char cmn_port_14;
+	unsigned char cmn_port_15;
+	unsigned char cmn_port_16;
+	unsigned char cmn_port_17;
+	unsigned char cmn_port_18;
+	unsigned char cmn_port_19;
+	unsigned char cmn_port_20;
+	unsigned char cmn_port_21;
+	unsigned char cmn_port_22;
+	unsigned char cmn_port_23;
+	unsigned char cmn_port_24;
+	unsigned char cmn_port_25;
+	unsigned char cmn_port_26;
+	unsigned char cmn_port_27;
+	unsigned char cmn_port_28;
+	unsigned char cmn_port_29;
+	unsigned char cmn_port_30;
+	unsigned char cmn_port_31;
+	unsigned char cmn_port_32;
+	unsigned char cmn_port_33;
+	unsigned char cmn_port_34;
+	unsigned char cmn_port_35;
+	unsigned char extended_addr_1;
+	unsigned char ext_cen_2;
+	unsigned char sp_pin0;
+	unsigned char sp_pin1;
 } mux_type;
 
 #define MUX ((mux_type*)SHARED_PINS_ADDR)
