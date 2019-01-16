5aa73b5e20df2b21ec5e368c35fb96e9d1fc5ccd
arnfol <arnfol@mail.ru>
2019-01-16 22:53:42 +0300

    read table and prepare enums

--- gen_c_header.py
+++ gen_c_header.py
@@ -14,13 +14,13 @@ enum_str = "enum {{ {isigs} }} {psig}_opt;"
 struct_str = "unsigned char {psig};"
 header_template = """
 /*
-	This is an automatically generated file.
+    This is an automatically generated file.
 
-	Date: {date}
-	Author: {author}
+    Date: {date}
+    Author: {author}
 
-	Usage:
-	MUX->{example_psig} = {example_psig}_opt.{example_isig};
+    Usage:
+    MUX->{example_psig} = {example_psig}_opt.{example_isig};
 
 */
 
@@ -44,7 +44,30 @@ typedef struct mux_type {{
 
 
 def main():
-	# ------------------------------------------------------------------------------------
+
+    psig_list, isig_list = gen_shared_pins.read_table(gen_shared_pins.inp_file, gen_shared_pins.head_lines)
+
+    enums = ""
+    for p in psig_list:
+        isigs = []
+
+        if p['bit'] is None:
+            psig = p['name']
+        else:
+            psig = p['name'] + '_' + p['bit']
+
+        for i in p['connections']:
+            if i is not None:
+                if i['bit'] is None:
+                    isigs.append(i['name'])
+                else:
+                    isigs.append(i['name'] + '_' + i['bit'])
+
+        enums += enum_str.format(psig=psig, isigs=', '.join(isigs)) + '\n'
+
+    print(enums+'\n')
+
+    # ------------------------------------------------------------------------------------
     # generate output file 
     # ------------------------------------------------------------------------------------
     print('Generating source file...')
@@ -52,18 +75,19 @@ def main():
     result = header_template.format(
         date=gen_shared_pins.date,
         author=gen_shared_pins.author,
-        example_psig=,
-        example_isig=,
+        example_psig="",
+        example_isig="",
         addr=shared_pins_apb_addr,
-        enums=,
-        registers=
+        enums="",
+        registers=""
         )
 
     with open(out_file,'w') as out:
         out.write(result)
+
     print('Done!')
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
---
+++ shared_pins.h
@@ -0,0 +1,28 @@
+
+/*
+    This is an automatically generated file.
+
+    Date: 2019-01-16 22:51
+    Author: arnfo
+
+    Usage:
+    MUX-> = _opt.;
+
+*/
+
+// shared_pins address
+#define SHARED_PINS_ADDR 0
+
+// ----------------------------------------------------------------
+// Register types
+// ----------------------------------------------------------------
+
+
+// ----------------------------------------------------------------
+// Registers
+// ----------------------------------------------------------------
+typedef struct mux_type {
+    
+} mux_type;
+
+#define MUX ((mux_type*)SHARED_PINS_ADDR)
